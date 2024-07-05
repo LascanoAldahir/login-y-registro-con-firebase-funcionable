6a322ea76da7d77c0e4a8524d40f09d4e00802d5
LascanoAldahir <aldahirlascano@gmail.com>
2024-07-05 15:15:48 -0500

    acabado multiplicacion

--- lib/firstscreen.dart
+++ lib/firstscreen.dart
@@ -4,21 +4,34 @@ import 'secondscreen.dart';
 class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Definimos dos números y su multiplicación
+    int number1 = 5;
+    int number2 = 7;
+    int result = number1 * number2;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Primera Pantalla'),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: Text('Ir a la Segunda Pantalla'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => SecondScreen(data: 'Hola desde la Primera Pantalla'),
-              ),
-            );
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Multiplicación: $number1 x $number2'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              child: Text('Ir a la Segunda Pantalla'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        SecondScreen(data: 'El resultado es: $result'),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
